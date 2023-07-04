1740083d87eb8337509d319755c2c717e25bb326
hl <1902144933@qq.com>
2023-07-04 12:47:20 +0800

    调整侧边栏

--- src/mock/setting.js
+++ src/mock/setting.js
@@ -1,20 +1,20 @@
 import Mock from "mockjs";
+import img from "@/assets/avatar.webp";
 Mock.mock("/api/setting", "get", {
   code: 0,
   msg: "",
   data: {
-    avatar: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+    // avatar: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+    avatar: img,
     siteTitle: "我的个人空间",
-    github: "https://github.com/DuYi-Edu",
-    qq: "3263023350",
-    qqQrCode:
-      "http://www.duyiedu.com/source/img/%E5%B0%8F%E6%B8%A1%E5%BE%AE%E4%BF%A1%E4%BA%8C%E7%BB%B4%E7%A0%81.png",
-    weixin: "yh777bao",
-    weixinQrCode:
-      "http://www.duyiedu.com/source/img/%E5%85%AC%E4%BC%97%E5%8F%B7%E4%BA%8C%E7%BB%B4%E7%A0%81.png",
-    mail: "duyi@gmail.com",
-    icp: "黑ICP备17001719号",
+    github: "https://github.com/Rookie9797/my-site",
+    qq: "1902144933",
+    qqQrCode: "",
+    weixin: "Alienes_",
+    weixinQrCode: "",
+    mail: "1902144933@qq.com",
+    icp: "备案号留空",
     githubName: "Rookie9797",
-    favicon: "http://mdrs.yuanjin.tech/Fs4CDlC6mwe_WXLMIiXcmSJLHO4f",
+    favicon: img,
   },
 });
